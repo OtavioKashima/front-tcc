fb264e32b6b4d87248eee33a5be6c053969133f9
Vieira16 <rafaelcevieira8@gmail.com>
2025-10-16 17:13:45 -0300

    merge

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-home',
+  standalone: false,
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
+})
+export class HomePage {
+  constructor(private navCtrl: NavController) {}
+
+  goToLogin() {
+    this.navCtrl.navigateForward('/login');
+  }
+
+  goToRegistration() {
+    this.navCtrl.navigateForward('/registro');
+  }
+}
