736562c1e3a2b3a019449593cdfaa5e84120f4b9
Otavio Kashima <43879875880@escola928.edu>
2025-10-16 17:04:37 -0300

    Principal Funcionando até a adoções

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,69 +1,40 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
-import { LoginPage } from './login/login.page';
-import { RegistroPage } from './registro/registro.page';
-import { AdocoesPage } from './adocoes/adocoes.page';
-import { DoacoesPage } from './doacoes/doacoes.page';
-import { PerfilPage } from './perfil/perfil.page';
-
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  },
-  {
     path: '',
-    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+    redirectTo: 'home',
+    pathMatch: 'full'
   },
   {
-    path: 'adocoes',
-    loadChildren: () => import('./adocoes/adocoes.module').then( m => m.AdocoesPageModule)
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
   },
   {
-    path: 'doacoes',
-    loadChildren: () => import('./doacoes/doacoes.module').then( m => m.DoacoesPageModule)
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule)
   },
   {
     path: 'registro',
-    loadChildren: () => import('./registro/registro.module').then( m => m.RegistroPageModule)
+    loadChildren: () => import('./registro/registro.module').then(m => m.RegistroPageModule)
+  },
+  {
+    path: 'adocoes',
+    loadChildren: () => import('./adocoes/adocoes.module').then(m => m.AdocoesPageModule)
+  },
+  {
+    path: 'doacoes',
+    loadChildren: () => import('./doacoes/doacoes.module').then(m => m.DoacoesPageModule)
   },
   {
     path: 'perfil',
-    loadChildren: () => import('./perfil/perfil.module').then( m => m.PerfilPageModule)
-  },
-
-
-
-
+    loadChildren: () => import('./perfil/perfil.module').then(m => m.PerfilPageModule)
+  }
 ];
+
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
-
-const tela1teste: Routes = [
-  {
-    path: './src/app/login/login.page.html',
-    component: LoginPage, 
-  },
-  {
-    path: './src/app/registro/registro.page.html',
-    component: RegistroPage,
-  },
-  {
-    path: './src/app/adocoes/adocoes.page.html',
-    component: AdocoesPage,
-  },
-  {
-    path: './src/app/doacoes/doacoes.page.html',
-    component: DoacoesPage,
-  },
-  {
-    path: './src/app/perfil/perfil.page.html',
-    component: PerfilPage,
-  },
-];
