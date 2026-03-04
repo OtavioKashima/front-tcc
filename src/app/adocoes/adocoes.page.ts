c599dbbeaf478dc0fa024ceb769df77cb83e4b75
OtavioKashima <otavioyuukikashima1@gmail.com>
2026-03-04 15:18:32 -0300

    Arrumando os conflitos

--- src/app/adocoes/adocoes.page.ts
+++ src/app/adocoes/adocoes.page.ts
@@ -4,7 +4,8 @@ import { Location } from '@angular/common';
 @Component({
   selector: 'app-adocoes',
   templateUrl: './adocoes.page.html',
-  styleUrls: ['./adocoes.page.scss']
+  styleUrls: ['./adocoes.page.scss'],
+  standalone: false
 })
 export class AdocoesPage {
 
